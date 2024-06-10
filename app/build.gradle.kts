2bce2317eec23d22560dd716f1960ed03ee5766f
ducnp98 <duc.nguyenphuoc160198@gmail.com>
2024-06-10 18:11:01 +0700

    10/6/2024: Set up bottom navigation bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kotlineasyrecipe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kotlineasyrecipe"
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +45,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- app/src/main/java/com/example/kotlineasyrecipe/MainActivity.kt
+++ app/src/main/java/com/example/kotlineasyrecipe/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlineasyrecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        var navController = Navigation.findNavController(this, R.id.frag_host)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
